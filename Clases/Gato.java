1c6697beda2efd787e61feba8279b7762917ec80
Franco <laimefrancoemanuel@gmail.com>
2021-09-29 23:45:30 -0300

    Arreglando las c4g4d45 de cierta persona

--- Clases/Gato.java
+++ Clases/Gato.java
@@ -1,1 +1,6 @@
-voy a modificar esto solo para que tardes mas
+// voy a modificar esto solo para que tardes mas
+// y yo voy a modificar esto tmb
+public interface Gato {
+	void maullar();
+}
+// soy lo mas
